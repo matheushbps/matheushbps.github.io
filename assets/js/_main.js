8cd0b4f3bb0602166227819bbf0794416caae896
matheushbps <matheushrullzs@gmail.com>
2024-11-05 15:01:50 -0300

    test toc v4

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,7 +1,3 @@
-/* ==========================================================================
-   jQuery plugin settings and other scripts
-   ========================================================================== */
-
 $(document).ready(function(){
   // Sticky footer
   var bumpIt = function() {
@@ -24,42 +20,16 @@ $(document).ready(function(){
   // FitVids init
   fitvids();
 
-  // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function() {
-    $(".author__urls").fadeToggle("fast", function() {});
-    $(".author__urls-wrapper button").toggleClass("open");
-  });
-
-  // init smooth scroll, this needs to be slightly more than then fixed masthead height
-  $("a").smoothScroll({offset: -65});
-
-  // add lightbox class to all image links
-  $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
-
-  // Magnific-Popup options
-  $(".image-popup").magnificPopup({
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
-    },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-zoom-in',
-    callbacks: {
-      beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
-        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+  // TOC Toggle functionality
+  document.addEventListener('DOMContentLoaded', function() {
+      const tocToggle = document.querySelector('.toc__toggle');
+      if (tocToggle) {
+          tocToggle.addEventListener('click', function() {
+              document.querySelector('.toc').classList.toggle('is-hidden');
+          });
       }
-    },
-    closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
+  // Follow menu drop down
+  // ... rest of the existing code ...
 });
